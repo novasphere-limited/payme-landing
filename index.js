e5b2e1f4ac9d4896a8bacddfd65b6c3b3d1366e1
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-07-29 19:59:14 +0100

    Algorithms continuation

--- index.js
+++ index.js
@@ -112,18 +112,90 @@ function whatIsInAName(collection, source) {
   }
   return newArr;
 }
-console.log(
-  whatIsInAName(
-    [
-      { first: "Romeo", last: "Montague" },
-      { first: "Mercutio", last: null },
-      { first: "Tybalt", last: "Capulet" },
-    ],
-    { last: "Capulet" }
-  )
-);
+// console.log(
+//   whatIsInAName(
+//     [
+//       { first: "Romeo", last: "Montague" },
+//       { first: "Mercutio", last: null },
+//       { first: "Tybalt", last: "Capulet" },
+//     ],
+//     { last: "Capulet" }
+//   )
+// );
 // console.log(
 //   whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], {
 //     apple: 1,
 //   })
 // );
+
+//GROUP ALGORITHMS ASSESSMENTS
+
+//Time complexity of O(n2);
+
+//Function declaration
+// function twoSum(arr, sum) {
+//   arr.sort((a, b) => {
+//     return a - b;
+//   });
+
+//   console.log(arr);
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     //value of j is dependent on the value of i
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === sum) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return [];
+// }
+//Time complexity of O(n);
+function twoSum(arr, sum) {
+  console.log(arr);
+  for (let i = 0; i < arr.length - 1; i++) {
+    const checker = sum - arr[i];
+    if (arr.includes(checker)) {
+      return [i, arr.indexOf(checker)];
+    }
+  }
+  return [];
+}
+// console.log(twoSum([2, 3, 4, 5], 7));
+// console.log(twoSum([2, 3, 4, 5], 9));
+// console.log(twoSum([25, 13, 14, 10], 24));
+
+//Arrow function
+const reverse = (int) => {
+  return Number((int + "").split("").reverse().join(""));
+};
+// console.log(reverse(123));
+
+//Function expression
+findMedianSortedArray = function (arr, arr1) {
+  let newArr = arr.concat(arr1).sort((a, b) => a - b);
+  //CHECKER FOR ODD
+  if (newArr.length % 2 != 0) return newArr[Math.ceil(newArr.length / 2)];
+  //cHECKER FOR EVEN
+  return (newArr[newArr.length / 2 - 1] + newArr[newArr.length / 2]) / 2;
+  // for(let i = 0; i < newArr.length; i++)
+  // return newArr;
+};
+
+// console.log(findMedianSortedArray([5, 6, 7], [4, 5, 6]));
+// console.log(findMedianSortedArray([5, 6, 7], [4, , 6]));
+
+const removeDuplicates = (arr) => {
+  // const distinct = new Set();
+  const rays = [];
+  const ray = [];
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (rays.includes(arr[i])) {
+      ray.push("*");
+    } else {
+      rays.push(arr[i]);
+    }
+  }
+  return rays.concat(ray);
+};
+console.log(removeDuplicates([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4]));
